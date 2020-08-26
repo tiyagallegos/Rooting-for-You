f7853cec62a6d33dee131dae0eec4a828815dd87
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-08-25 23:04:57 -0600

    styled and completed functions of lab

--- rootingforyou/settings.py
+++ rootingforyou/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'rootingforyou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rootingforyou',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
