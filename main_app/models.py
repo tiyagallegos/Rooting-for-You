18e71cf8a6e208925d2f47b079a0066e7b8fff73
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-08-26 21:08:22 -0600

    created model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    sci_name = models.CharField(max_length=100)
+    common_name = models.CharField(max_length=100)
+    water = models.TextField(max_length=250)
+    sun = models.TextField(max_length=250)
+    food = models.TextField(max_length=250)
+    toxicity = models.CharField(max_length=100)
+    age = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
