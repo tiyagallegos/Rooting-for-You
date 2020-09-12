ca4a7f29700f95f89f10a1e97aec59a344bdb1bc
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-09-12 13:57:44 -0600

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rootingforyou.wsgi
--- rootingforyou/settings.py
+++ rootingforyou/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -121,3 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/plants/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+#Prodution settings -Uncomment before deploying to heroku
+
+#import django_heroku
+#django_heroku.settings(locals())
+
+###
